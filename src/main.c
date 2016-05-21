6cd4859ab64e9bf2ad8b190968fefd0c40a3542a
reenx <erik.rakhorst@gmail.com>
2016-05-21 09:59:08 +0200

    lap detection
    lap detectie door twee snelle slagen.
    layout en font iets aangepast

--- src/main.c
+++ src/main.c
@@ -10,21 +10,45 @@ int LapCount = 0;
 CompassHeadingData compasdata;
 long int heading = 999;
 static char strokecounttext[5];
-static char lapcounttext[5];
-static char stroketimetext[10];
+static char lapcounttext[10];
+static char stroketimetext[11];
+time_t lap_seconds = 0;
+uint16_t lap_milliseconds = 0;
 time_t new_seconds = 0;
 uint16_t new_milliseconds = 0;
 time_t prev_seconds = 0;
 uint16_t prev_milliseconds = 0;
 
-uint16_t time_diff_ms() {
-   uint16_t diff_milliseconds;
-   time_t diff_seconds = new_seconds - prev_seconds;
+uint32_t time_diff_ms(time_t base_seconds, uint16_t base_milliseconds) {
+   uint32_t diff_milliseconds = 0;
+   time_t diff_seconds = 0 ;
+   if (new_seconds > base_seconds){
+      diff_seconds  = new_seconds - base_seconds;
+   }
    diff_milliseconds = diff_seconds * 1000;
-   return diff_milliseconds + new_milliseconds - prev_milliseconds;
+   diff_milliseconds = diff_milliseconds + new_milliseconds;
+   if (diff_milliseconds > base_milliseconds){
+      diff_milliseconds = diff_milliseconds - base_milliseconds;
+   } else {
+      diff_milliseconds = 0;
+   }
+   return diff_milliseconds;
 }
 
 static void accel_data_handler(AccelData *data, uint32_t num_samples) {
+   
+   time_ms(&new_seconds, &new_milliseconds);
+   if (prev_seconds == 0) {
+      prev_seconds = new_seconds;
+      prev_milliseconds = new_milliseconds;
+      lap_seconds = new_seconds;
+      lap_milliseconds = new_milliseconds;
+   }
+   uint32_t laptime = time_diff_ms(lap_seconds, lap_milliseconds);
+//    APP_LOG(APP_LOG_LEVEL_INFO, "=== %ld = %d ===", lap_seconds, lap_milliseconds);
+
+   snprintf(stroketimetext, sizeof(stroketimetext),"%li", laptime);
+   text_layer_set_text(stroke_time_layer, stroketimetext);
 
    for (int i=0; i < (int)num_samples; i++){
    
@@ -36,15 +60,6 @@ static void accel_data_handler(AccelData *data, uint32_t num_samples) {
 //                  data[i].y,
 //                  data[i].z);
 
-         if ((data[i].y < 250) && (data[i].y > -250)) {
-            //get compass data
-            compass_service_peek(&compasdata);
-            if (compasdata.compass_status == CompassStatusCalibrated) {
-               heading = TRIGANGLE_TO_DEG(compasdata.magnetic_heading);
-            } else {
-               heading = 999;
-            }
-         }
 
          if(data[i].y > 500) {
             StrokeStatus = 0;
@@ -54,13 +69,12 @@ static void accel_data_handler(AccelData *data, uint32_t num_samples) {
             StrokeCount++;
             snprintf(strokecounttext, sizeof(strokecounttext),"%d", StrokeCount);
             text_layer_set_text(stroke_count_layer, strokecounttext);
-            time_ms(&new_seconds, &new_milliseconds);
             if (prev_milliseconds != 0) {
-               uint16_t stroketime = time_diff_ms();
-               snprintf(stroketimetext, sizeof(stroketimetext),"%d", stroketime);
-               text_layer_set_text(stroke_time_layer, stroketimetext);
+               uint32_t stroketime = time_diff_ms(prev_seconds, prev_milliseconds);
                
-               if (stroketime > 6000){
+               if ((stroketime < 1000) && (laptime >10000)){
+                  lap_seconds = new_seconds;
+                  lap_milliseconds = new_milliseconds;
                   LapCount++;
                   snprintf(lapcounttext, sizeof(lapcounttext),"%d", LapCount);
                   text_layer_set_text(lap_count_layer, lapcounttext);
@@ -77,7 +91,7 @@ static void accel_data_handler(AccelData *data, uint32_t num_samples) {
          APP_LOG(APP_LOG_LEVEL_WARNING, "Vibration occured during collection");
       }
    }
-   APP_LOG(APP_LOG_LEVEL_INFO, "=== %d = %ld ===", StrokeCount, heading);
+   APP_LOG(APP_LOG_LEVEL_INFO, "=== %d ===", StrokeCount);
 }
 
 
@@ -86,18 +100,18 @@ void handle_init(void) {
    uint32_t num_samples = 5;  // Number of samples per batch/callback
    my_window = window_create();
 
-   stroke_count_layer = text_layer_create(GRect(12, 20, 120, 40));
-   stroke_time_layer = text_layer_create(GRect(12, 65, 120, 40));
-   lap_count_layer = text_layer_create(GRect(12, 110, 120, 40));
+   stroke_count_layer = text_layer_create(GRect(5, 10, 135, 40));
+   stroke_time_layer = text_layer_create(GRect(5, 60, 135, 40));
+   lap_count_layer = text_layer_create(GRect(5, 110, 135, 40));
    text_layer_set_text(stroke_count_layer, "000");
    text_layer_set_text(stroke_time_layer, "0000");
    text_layer_set_text(lap_count_layer, "0");
    text_layer_set_text_alignment(stroke_count_layer, GTextAlignmentRight);
    text_layer_set_text_alignment(stroke_time_layer, GTextAlignmentRight);
    text_layer_set_text_alignment(lap_count_layer, GTextAlignmentRight);
-   text_layer_set_font(stroke_count_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+   text_layer_set_font(stroke_count_layer, fonts_get_system_font(FONT_KEY_LECO_32_BOLD_NUMBERS));
    text_layer_set_font(stroke_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-   text_layer_set_font(lap_count_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+   text_layer_set_font(lap_count_layer, fonts_get_system_font(FONT_KEY_LECO_32_BOLD_NUMBERS));
    text_layer_set_background_color(stroke_count_layer, GColorGreen);
    text_layer_set_background_color(lap_count_layer, GColorGreen);
    text_layer_set_background_color(stroke_time_layer, GColorYellow);
